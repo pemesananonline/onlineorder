46bf83b150557fa7344b19e282da5a29678e514f
pemesananonline <pemesananemail@gmail.com>
2026-05-15 13:28:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 /* ================= SERVICE WORKER CACHE =============== */
 /* ===================================================== */
 
-const CACHE_NAME = "order-online-cache-v21";
+const CACHE_NAME = "order-online-cache-v22";
 
 const FILES_TO_CACHE = [
     "./",
